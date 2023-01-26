897ae86abd8819a9541d3c03b6521b07b03eec28
ssvinko <nicodae86@gmail.com>
2023-01-25 19:33:17 -0500

    clase1

--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'categories.apps.CategoriesConfig',
+    'authors.apps.AuthorsConfig',
+    'books.apps.BooksConfig',
 ]
 
 MIDDLEWARE = [
